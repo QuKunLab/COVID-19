e958d70de890d7e899287df946db3b0c057d0f6d
QuKunLab <libin03@ustc.edu.cn>
2020-05-13 18:16:10 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # COVID-19
 The R/Python scripts for the analysis of single-cell RNA-seq data from COVID-19 patients
+
+## 1. Requirement
+We analyzed the scRNA-seq data in a Linux system with R (version 3.6.1) and Python (version 3.6.8) enviroment. The following software and packages are also required:
+
+software|version|enviroment
+-|-|-
+Cellranger|3.1.0|Linux
+Seurat|3.1.4|R
+dplyr|0.8.4|R
+patchwork|1.0.0|R
+scrublet|0.2.1|Python
+scipy|1.0.0|Python
+pandas|0.24.2|Python
+matplotlib|3.0.3|Python
+seaborn|0.9.0|Python
---
+++ step0_map_raw_data_using_cellranger.sh
@@ -0,0 +1,8 @@
+cellranger count --id=P1-1r1_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P1-1r1/
+cellranger count --id=P1-1r2_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P1-1r2/
+cellranger count --id=P1-2r1_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P1-2r1/
+cellranger count --id=P1-2r2_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P1-2r2/
+cellranger count --id=P2-1_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P2-1/
+cellranger count --id=P2-2_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P2-2/
+cellranger count --id=P2-3_outs --transcriptome=~/refdata-cellranger-GRCh38-3.0.0/ --fastqs=./P2-3/
+cellranger aggr --id=COVID19 --csv=COVID19.csv --normalize=mapped
